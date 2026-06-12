b4cbc21d85073150a98f2b39915dd979326bfe23
pineapple <harjapkapoor22@gmail.com>
2026-06-12 15:36:10 +0530

    lets see if this works

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import discord
 from discord.ext import commands
 import os
 from dotenv import load_dotenv
+import aiosqlite
 
 load_dotenv()
 
@@ -23,10 +24,26 @@ presence = discord.Game("modpack release soon??")
 async def on_ready():
     print(f"✅ Logged in as {bot.user} ({bot.user.id})")
     await bot.change_presence(status=discord.Status.online, activity=presence)
+    bot.db = await aiosqlite.connect("tags.db")
+    async with bot.db.cursor() as cursor:
+        await cursor.execute("CREATE TABLE IF NOT EXISTS tags (name TEXT, content TEXT, guild INTEGER, creator INTEGER)")
 
 @bot.command()
 async def ping(ctx):
     await ctx.send("Pong!")
+
+@bot.command(name="t add", description="Creates a new tag")
+async def create_tag(ctx: commands.Context, name: str, *, content: str):
+    async with bot.db.cursor() as cursor:
+        await cursor.execute("SELECT content FROM tags WHERE guilld = ? AND name = ?", (ctx.guild.id, name))
+        data = await cursor.fetchone()
+        if data is None: 
+            await cursor.execute("INSERT INTO tags (name, content, guild, creator) VALUES (?, ?, ?, ?)", (name, content, ctx.guild.id, ctx.author.id))
+            await ctx.send("✅ Tag created successfully!")
+        if data:
+            await ctx.send("❌ Tag already exists!")
+
+        await bot.db.commet()
 
 async def main():
     async with bot:
